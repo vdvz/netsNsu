ca256c6e0cbd8722b0342894961b32ea84576efa
vdvz <vizir.vadim@mail.ru>
2020-09-22 18:40:21 +0700

    done

--- lab1/src/Client.java
+++ lab1/src/Client.java
@@ -12,18 +12,16 @@ public class Client extends Thread{
     private final ActionListener sendWelcome = e -> sendWelcomeMessage();
     private final ActionListener checkClients = e -> checkIfClientAlive();
 
-
     private  MulticastSocket s;
-    private  MulticastSocket r;
-    private  SocketAddress socketAddr;
-
+    private String ip;
+    private static final int port = 6789;
+    private InetAddress groupAddress;
 
     private  void sendWelcomeMessage(){
         byte[] buf = new byte[1000];
         buf[0] = 1;
         DatagramPacket welcome = new DatagramPacket(buf, buf.length, groupAddress, port);
         welcome.setData(buf);
-        //welcome.setSocketAddress(socketAddr);
         try {
             s.send(welcome);
             System.out.println("Message send");
@@ -32,24 +30,16 @@ public class Client extends Thread{
         }
     }
 
-    String ip;
     public void setIp(String _ip){
         ip = _ip;
     }
-    InetAddress groupAddress;
-    int port = 6789;
+
     public void run(){
         System.out.println("My: " + ip);
-        int port1 = 6789;
-        int port2 = 6790;
         try {
             groupAddress = InetAddress.getByName(ip);
-            s = new MulticastSocket(6789);
-            r = new MulticastSocket(port2);
-            //socketAddr = new InetSocketAddress(groupAddress, port1);
-            //SocketAddress socketAddr2 = new InetSocketAddress(groupAddress, port2);
+            s = new MulticastSocket(port);
             s.joinGroup(groupAddress);
-            r.joinGroup(groupAddress);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,24 +49,22 @@ public class Client extends Thread{
         timer1.start();
         timer2.start();
 
-        //while(true){
-        byte[] buf = new byte[1000];
-        DatagramPacket dp = new DatagramPacket(buf, buf.length);
-        try {
-            System.out.println("try block");
-            s.receive(dp);
-            System.out.println("receive");
-        } catch (IOException e) {
-            e.printStackTrace();
+        while(true){
+            byte[] buf = new byte[1000];
+            DatagramPacket dp = new DatagramPacket(buf, buf.length);
+            try {
+                s.receive(dp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String host = dp.getAddress().getHostName();
+            if(!aliveClients.containsKey(host)){
+                System.out.println("Client connected: " + host);
+                aliveClients.put(host, 1);
+            }else {
+                aliveClients.replace(host, aliveClients.get(host) + 1);
+            }
         }
-        String host = dp.getAddress().getHostName();
-        if(!aliveClients.containsKey(host)){
-            System.out.println("Client connected: " + host);
-            aliveClients.put(host, 1);
-        }else {
-            aliveClients.replace(host, aliveClients.get(host) + 1);
-        }
-        //}
     }
 
     private  void checkIfClientAlive(){
